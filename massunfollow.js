2b3f30000e92bf212ec3bf886322a659c376178e
Marakuja.eu <36244959+marakujaeu@users.noreply.github.com>
2019-05-10 20:43:33 +0200

    Bring some logic to the magic

--- massunfollow.js
+++ massunfollow.js
@@ -1,6 +1,14 @@
 (function massunfollow() {
-    document.getElementsByClassName('_8A5w5')[3].click();document.getElementsByClassName('aOOlW -Cab_')[0].click();
-    var randomTimeoutForUnfollow = Math.floor((Math.random() * 15) + 5) * 1000;
+    if(document.getElementsByClassName('aOOlW -Cab_')[0]){
+
+    document.getElementsByClassName('_8A5w5')[3].click();
+    document.getElementsByClassName('aOOlW -Cab_')[0].click();
+
+    } else {
+
+        document.getElementsByClassName('_8A5w5')[3].click();
+    }
+    var randomTimeoutForUnfollow = Math.floor((Math.random() * 10) + 10) * 1000;
     console.log(randomTimeoutForUnfollow);
         setTimeout(massunfollow, randomTimeoutForUnfollow);
 })();
