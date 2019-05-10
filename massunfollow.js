7e8896383d8d9edd8aff9a126e1d325243d2d664
Marakuja.eu <36244959+marakujaeu@users.noreply.github.com>
2019-05-10 17:33:01 +0200

    Create massunfollow.js

---
+++ massunfollow.js
@@ -0,0 +1,6 @@
+(function massunfollow() {
+    document.getElementsByClassName('_8A5w5')[3].click();document.getElementsByClassName('aOOlW -Cab_')[0].click();
+    var randomTimeoutForUnfollow = Math.floor((Math.random() * 15) + 1) * 1000;
+    console.log(randomTimeoutForUnfollow);
+        setTimeout(massunfollow, randomTimeoutForUnfollow);
+})();
