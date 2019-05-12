983588684d7c92fb75bbc771b3f6db2e4f900710
Marakuja.eu <36244959+marakujaeu@users.noreply.github.com>
2019-05-13 00:30:17 +0200

    Remove line added by mistake and introduce simple scrolling for loading new followers

--- massunfollow.js
+++ massunfollow.js
@@ -1,14 +1,16 @@
+toppos = 0;
 (function massunfollow() {
     if(document.getElementsByClassName('aOOlW -Cab_')[0]){
 
-    document.getElementsByClassName('_8A5w5')[3].click();
     document.getElementsByClassName('aOOlW -Cab_')[0].click();
 
     } else {
-
+        toppos = toppos+30;
         document.getElementsByClassName('_8A5w5')[3].click();
+        document.getElementsByClassName('isgrP')[0].scrollTop = toppos;
+        
     }
-    var randomTimeoutForUnfollow = Math.floor((Math.random() * 10) + 10) * 1000;
+    var randomTimeoutForUnfollow = Math.floor((Math.random() * 10) + 9) * 1000;
     console.log(randomTimeoutForUnfollow);
         setTimeout(massunfollow, randomTimeoutForUnfollow);
 })();
